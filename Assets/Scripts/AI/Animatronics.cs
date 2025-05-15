ef17adb15dfb4b83946e1047d763f7970d445f90
Bas de Reus <70578065+bas-boop@users.noreply.github.com>
2025-05-15 19:19:40 +0200

    Added AnimatronicBuilder

---
+++ Assets/Scripts/AI/Animatronics.cs
@@ -0,0 +1,48 @@
+﻿using StateMachine;
+
+namespace AI
+{
+    public class Animatronic
+    {
+        private SM _stateMachine;
+
+        private string _name;
+        private int _startCamera;
+        private int _respawnCamera;
+        private int _level;
+        
+        public class AnimatronicBuilder
+        {
+            private readonly Animatronic _animatronic = new ();
+
+            public AnimatronicBuilder SetName(string name)
+            {
+                _animatronic._name = name;
+                return this;
+            }
+            
+            public AnimatronicBuilder SetStartCamera(int startCam)
+            {
+                _animatronic._startCamera = startCam;
+                return this;
+            }
+            
+            public AnimatronicBuilder SetRespawnCamera(int respawnCam)
+            {
+                _animatronic._respawnCamera = respawnCam;
+                return this;
+            }
+            
+            public AnimatronicBuilder SetLevel(int level)
+            {
+                _animatronic._level = level;
+                return this;
+            }
+
+            public Animatronic Build()
+            {
+                return _animatronic;
+            }
+        }
+    }
+}
--- Assets/Scripts/TheGame.cs
+++ Assets/Scripts/TheGame.cs
@@ -4,48 +4,23 @@ using UnityEngine;
 public sealed class TheGame : MonoBehaviour
 {
     private Enemy _goblin;
-    private float _time;
+    private Animatronic _freddy;
 
     private void Start()
     {
         _goblin = new Enemy();
         _goblin.Start();
+
+        _freddy = new Animatronic.AnimatronicBuilder()
+            .SetName("Freddy")
+            .SetStartCamera(2)
+            .SetRespawnCamera(3)
+            .SetLevel(5)
+            .Build();
     }
 
     private void Update()
     {
-        _time += Time.deltaTime;
-
-        if (_time >= 24)
-            _time = 0;
         
-        if (Almost(_time, 8))
-        {
-            _goblin.ChangeBehaviour(0);
-        }
-        else if (Almost(_time, 9))
-        {
-            _goblin.ChangeBehaviour(1);
-        }
-        else if (Almost(_time, 12))
-        {
-            _goblin.ChangeBehaviour(0);
-        }
-        else if (Almost(_time, 18))
-        {
-            _goblin.ChangeBehaviour(1);
-        }
-        else if (Almost(_time, 21))
-        {
-            _goblin.ChangeBehaviour(2);
-        }
-    }
-
-    private bool Almost(float time, float target)
-    {
-        const float margin = 0.01f;
-
-        return time + margin >= target
-               && time - margin <= target;
     }
 }
