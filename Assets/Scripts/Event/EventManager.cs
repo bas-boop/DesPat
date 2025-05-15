2686a8916380592e8db7fed3d25e88a8298c849c
Bas de Reus <70578065+bas-boop@users.noreply.github.com>
2025-05-15 23:57:38 +0200

    Added codeconvention

--- Assets/Scripts/Event/EventManager.cs
+++ Assets/Scripts/Event/EventManager.cs
@@ -5,26 +5,26 @@ namespace Event
 {
     public static class EventManager
     {
-        private static readonly Dictionary<EventType, Action> EvenDict = new();
+        private static readonly Dictionary<EventType, Action> _eventDict = new();
 
         public static void AddListener(EventType type, Action func)
         {
-            EvenDict.TryAdd(type, null); // checks of the type is already in the dict or not
-            EvenDict[type] += func;
+            _eventDict.TryAdd(type, null); // checks of the type is already in the dict or not
+            _eventDict[type] += func;
         }
         
         public static void RemoveListener(EventType type, Action func)
         {
-            if (EvenDict.ContainsKey(type)
-                && EvenDict[type] != null)
+            if (_eventDict.ContainsKey(type)
+                && _eventDict[type] != null)
             {
-                EvenDict[type] -= func;
+                _eventDict[type] -= func;
             }
         }
         
         public static void InvokeEvent(EventType type)
         {
-            EvenDict[type]?.Invoke();
+            _eventDict[type]?.Invoke();
         }
     }
 }
